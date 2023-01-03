e2fc04a0d640a53b680500877f23afda80dbd7c2
baeian <ddoory1103@gmail.com>
2023-01-03 22:24:07 +0900

    stateHook

--- HookPractice/statehook/src/App.js
+++ HookPractice/statehook/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const init = () => {
+		console.log("init~~");
+		return ["홍길동", "김석진"];
+	};
+
+	const [names, setNames] = useState(() => {
+		return init();
+	});
+	const [input, setInput] = useState("");
+
+	const handleInputChange = e => {
+		setInput(e.target.value);
+	};
+
+	const handleUpload = () => {
+		setNames(prevState => {
+			console.log("이전 state: ", prevState);
+			return [input, ...prevState]; //[...]은 스프레드 문법으로 시작 주소를 복사하는게 아니라 배열 데이터 자체를 복사해줌
+		});
+	};
+
+	// console.log(input);
+
+	return (
+		<div>
+			<input type="text" value={input} onChange={handleInputChange} />
+			<button onClick={handleUpload}>Upload</button>
+			{names.map((names, index) => {
+				return <p key={index}>{names}</p>;
+			})}
+		</div>
+	);
 }
 
 export default App;

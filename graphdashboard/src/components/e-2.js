d41f825e20b911cc3a465e8cd28e830b5e8f5339
baeian <ddoory1103@gmail.com>
2023-01-10 12:05:19 +0900

    그래프 애니메이션 추가 및 수치 출력

--- graphdashboard/src/components/e-2.js
+++ graphdashboard/src/components/e-2.js
@@ -1,9 +1,10 @@
 import * as d3 from "d3";
+import { transition } from "d3";
 import { useEffect, useRef } from "react";
 
 const E2 = () => {
 	const canvasRef = useRef();
-	const data = [100, 10, 30, 50, 110, 80];
+	const data = [100, 10, 30, 50, 110, 80, 40];
 
 	useEffect(() => {
 		const canvas = d3.select(canvasRef.current);
@@ -15,10 +16,22 @@ const E2 = () => {
 
 		data.forEach((item, index) => {
 			svg.append("rect")
+				.attr("id", `rect-${index}`)
 				.attr("width", 20)
-				.attr("height", item)
+				.attr("height", 0)
 				.attr("x", index * 30)
+				.attr("y", 150)
+				.transition()
+				.duration(500)
+				.attr("height", item)
 				.attr("y", 150 - item);
+
+			svg.append("text")
+				.attr("x", index * 30)
+				.attr("y", 140 - item)
+				.text(item);
+			
+			d3.select('#rect-4').style("fill","blue");
 		});
 	}, []);
 
